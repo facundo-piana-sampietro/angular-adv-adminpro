e5bd75dc1ec48e27f9b27ba7801138283ba0b441
Facundo Piana Sampietro <facusampi@hotmail.com>
2024-08-07 16:30:12 -0300

    Fin sección 8

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -11,8 +11,10 @@ export class SidebarService {
       icon: 'mdi mdi-gauge',
       submenu: [
         {titulo: 'Main', url: '/'},
-        {titulo: 'ProgressBar', url: 'progress'},
         {titulo: 'Gráficas', url: 'grafica1'},
+        {titulo: 'ProgressBar', url: 'progress'},
+        {titulo: 'Promesas', url: 'promesas'},
+        {titulo: 'RXJS', url: 'rxjs'},
       ]
     }
   ]
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styles: ``
+  styles: ``,
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy{
+  public titulo: string = '';
+  public tituloSubs$: Subscription;
 
+  constructor(private router: Router) {
+    this.tituloSubs$ = this.getArgumentosRuta()
+      .subscribe(({ titulo }) => {
+       this.titulo = titulo;
+       document.title = titulo;
+    });
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getArgumentosRuta() {
+    return this.router.events.pipe(
+      filter(
+        (event) =>
+          event instanceof ActivationEnd && event.snapshot.firstChild === null
+      ),
+      map((event: any) => event.snapshot.data as any)
+    );
+  }
 }
